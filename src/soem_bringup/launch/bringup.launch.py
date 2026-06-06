120e5e71472faa955653d23e36915cb6b23c8b22
HappyDog060713 <gzqtbh20060713@163.com>
2026-06-06 16:54:44 +0800

    feat: init volleyball robot workspace

---
+++ src/soem_bringup/launch/bringup.launch.py
@@ -0,0 +1,26 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+import os
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    config_file = os.path.join(
+        get_package_share_directory('soem_bringup'),
+        'config',
+        'config-inf3.yaml'
+    )
+
+    return LaunchDescription([
+        Node(
+            package='soem_wrapper',
+            executable='soem_backend',
+            name='soem_backend',
+            parameters=[{
+                'interface': "enp2s0",
+                'rt_cpu': 4,
+                'non_rt_cpus': "0-3,5-11",
+                'config_file': config_file
+            }],
+            output='screen'
+        )
+    ])
